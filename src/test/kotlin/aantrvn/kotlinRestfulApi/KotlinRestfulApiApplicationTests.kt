cadcab0c4f66433c01f3380113763a5107ee0f60
aaneverything <aanvinanta5@gmail.com>
2024-09-07 10:36:02 +0700

    oh oh

---
+++ src/test/kotlin/aantrvn/kotlinRestfulApi/KotlinRestfulApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package aantrvn.kotlinRestfulApi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinRestfulApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
